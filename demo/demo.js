32f7a40f978aa387da0dbde3591bd1dad35dbee6
Mike Cantelon <mcantelon@gmail.com>
2012-04-16 17:36:28 -0700

    Added re-binding of drag-and-drop functionality in demo.

--- demo/demo.js
+++ demo/demo.js
@@ -101,8 +101,14 @@ function demo() {
     fileExplorer.busy();
     $('#message').text('User clicked upload link.');
     setTimeout(function() {
+      // render new directory structure
       fileExplorer.structure = dirSlightlyAlteredAndInJSON;
       fileExplorer.render();
+
+      // re-bind drag-and-drop behavior
+      $('.directory:not(:first)').bind('drag', dragHandler);
+      $('.directory:not(:first)').bind('drop', dropHandler);
+
       fileExplorer.idle();
       $('#message').text('');
     }, 1000);
